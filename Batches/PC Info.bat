6009d7e302b9025ad3867c863020226a9829287f
nlbence <40839895+nlbence@users.noreply.github.com>
2020-05-13 22:38:06 +0200

    Hid press any key text

--- Batches/PC Info.bat
+++ Batches/PC Info.bat
@@ -20,4 +20,4 @@ echo Other info
 echo.
 ver
 systeminfo | findstr /C:"OS Name" /C:"OS Version" /C:"BIOS Version" /C:"System Locale" /C:"System Type" /C:"System Manufacturer" /C:"System Model" /C:"Registered Owner" /C:"Registered Organisation" /C:"OS Build Type" /C:"OS Configuration" /C:"Host Name" /C:"Total Physical Memory" /C:"Time Zone" /C:"Boot Device" /C:"Original Install Date" /C:"System Boot Time" /C:"Processor(s)" /C:"Network Card(s)" /C:"Hyper-V Requirements"
-pause
+pause >nul
